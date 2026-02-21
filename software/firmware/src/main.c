bd62c268ffd659f8f6d83f6bfd5deaad4049caa5
Eryk Możdżeń <ermozdzen@gmail.com>
2026-02-21 15:02:56 +0100

    blink project

--- software/firmware/src/main.c
+++ software/firmware/src/main.c
@@ -1,49 +1,29 @@
-#include <focus.h>
+#include <FreeRTOS.h>
+#include <task.h>
 
-#include "driver.h"
+#include <stm32h5xx_hal.h>
 
-int main(void) {
+void SystemClock_Config();
+void MX_GPIO_Init();
 
-    driver_flash_t driver_flash;
-    const focus_memory_t memory = {
-        .user = &driver_flash,
-        .reset = driver_flash_reset,
-        .read = driver_flash_read,
-        .write = driver_flash_write,
-        .flush = driver_flash_flush,
-    };
+static StaticTask_t task;
+static StackType_t stack[1024];
 
-    driver_inverter_t driver_inverter;
-    const focus_inverter_t inverter = {
-        .user = &driver_inverter,
-        .reset = driver_inverter_reset,
-        .set_gates = driver_inverter_set_gates,
-        .get_current = driver_inverter_get_current,
-    };
-
-    driver_as5600_t driver_as5600;
-    const focus_encoder_t encoder = {
-        .user = &driver_as5600,
-        .reset = driver_as5600_reset,
-        .sample_start = driver_as5600_sample_start,
-        .sample_get = driver_as5600_sample_get,
-    };
-
-    uint8_t channel_buffer[FOCUS_CHANNEL_SIZE];
-    focus_channel_t channel;
-    focus_channel_create(&channel, channel_buffer, &inverter, &encoder);
-
-    uint8_t kernel_buffer[FOCUS_KERNEL_SIZE];
-    focus_kernel_t kernel;
-    focus_kernel_create(&kernel, kernel_buffer, &memory, &channel, 1);
-
-    focus_kernel_start(kernel);
-
+static void function(void *params) {
     while(1) {
-        focus_kernel_dispatch(kernel);
-
-        focus_channel_position_setpoint_set(channel, 1.f);
+        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_4);
+        vTaskDelay(500);
     }
+}
+
+int main() {
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+
+    xTaskCreateStatic(function, "blink", 1024, NULL, 1, stack, &task);
+
+    vTaskStartScheduler();
 
     return 0;
 }
