9f3c69f9654a1c5b043838cd88194765ea6ee887
Eryk Możdżeń <ermozdzen@gmail.com>
2026-05-17 11:53:52 +0200

    better scope.py app

--- example/scope.py
+++ example/scope.py
@@ -8,61 +8,85 @@
 # ]
 # ///
 
+import threading
 import paho.mqtt.client as mqtt
 import msgpack
 import matplotlib.pyplot as plt
 
-series = {
-    "time": [],
-    "signal_1": [],
-    "signal_2": [],
-    "signal_3": [],
-}
+data_accumulator = []
+data_plot = []
+lock = threading.Lock()
 
 plt.ion()
-fig = plt.figure()
-ax = fig.add_subplot(111)
+fig, ax = plt.subplots()
 line1, = ax.plot([], [], label="signal 1", linewidth=1)
 line2, = ax.plot([], [], label="signal 2", linewidth=1)
 line3, = ax.plot([], [], label="signal 3", linewidth=1)
+scatter1 = ax.scatter([], [], s=10, color=line1.get_color())
+scatter2 = ax.scatter([], [], s=10, color=line2.get_color())
+scatter3 = ax.scatter([], [], s=10, color=line3.get_color())
 ax.legend()
 ax.grid()
 
-def on_message(client, userdata, msg):
-    try:
-        data = msgpack.unpackb(msg.payload, raw=False)
-        index = int(data["index"])
-        print(index)
-        if index == 0:
-            line1.set_xdata(series["time"])
-            line2.set_xdata(series["time"])
-            line3.set_xdata(series["time"])
-            line1.set_ydata(series["signal_1"])
-            line2.set_ydata(series["signal_2"])
-            line3.set_ydata(series["signal_3"])
-            ax.relim()
-            ax.autoscale_view()
-            fig.canvas.draw()
-            fig.canvas.flush_events()
+def on_message(client, userdata, message):
+    global data_accumulator
+    global data_plot
 
-            series["time"] = []
-            series["signal_1"] = []
-            series["signal_2"] = []
-            series["signal_3"] = []
-        series["time"].extend([0.0001 * (data["index"] + i) for i in range(10)])
-        series["signal_1"].extend(data["signal_1"])
-        series["signal_2"].extend(data["signal_2"])
-        series["signal_3"].extend(data["signal_3"])
+    try:
+        message = msgpack.unpackb(message.payload)
+
+        if message["timestamp"] == 0:
+            with lock:
+                data_plot = data_accumulator.copy()
+            data_accumulator.clear()
+
+        for i in range(len(message["signal_1"])):
+            data_accumulator.append({
+                "timestamp": message["timestamp"] + (0.0001 * i),
+                "signal_1": message["signal_1"][i],
+                "signal_2": message["signal_2"][i],
+                "signal_3": message["signal_3"][i],
+            })
+
     except Exception as e:
-        print(f"invalid message: {msg.payload} ({e})")
+        print(e)
+
+def update_plot():
+    global data_plot
+
+    with lock:
+        timestamp = [item["timestamp"] for item in data_plot]
+        signal_1 = [item["signal_1"] for item in data_plot]
+        signal_2 = [item["signal_2"] for item in data_plot]
+        signal_3 = [item["signal_3"] for item in data_plot]
+
+    line1.set_data(timestamp, signal_1)
+    line2.set_data(timestamp, signal_2)
+    line3.set_data(timestamp, signal_3)
+
+    if timestamp:
+        scatter1.set_offsets(list(zip(timestamp, signal_1)))
+        scatter2.set_offsets(list(zip(timestamp, signal_2)))
+        scatter3.set_offsets(list(zip(timestamp, signal_3)))
+
+    ax.relim()
+    ax.autoscale_view()
+
+    fig.canvas.draw_idle()
+
+timer = fig.canvas.new_timer(interval=100)
+timer.add_callback(update_plot)
+timer.start()
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_message = on_message
 client.connect("localhost", 1883, 60)
 client.subscribe("focus/scope")
 
+client.loop_start()
+
 try:
-    client.loop_forever()
+    plt.show(block=True)
 except KeyboardInterrupt:
     client.loop_stop()
     client.disconnect()
